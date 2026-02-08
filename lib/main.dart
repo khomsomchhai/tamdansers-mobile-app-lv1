b3f39ffe08f11d64947ce83813bf7e3d608e0db8
Khom Somchhai <somchhai.khom@gmail.com>
2026-02-09 01:32:03 +0700

    just commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MainApp extends StatelessWidget {
             elevation: 0,
             surfaceTintColor: AppColors.transparent,
           )),
-      initialRoute: AppRoutes.teacherDashboard,
+      initialRoute: AppRoutes.manageStudentScreen,
       routes: PageRoutes.routes,
       home: Scaffold(),
     );
